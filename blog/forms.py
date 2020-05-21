194764157571752d140b55dd57759c41aa9e0093
KJM891 <kjm891@student.bham.ac.uk>
2020-05-21 08:57:11 +0100

    comments

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,18 @@
 from django import forms
-from .models import Post
+from django.forms import Textarea
+from .models import Post, Comment
 
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('author', 'text',)
+        widgets = {
+            'text': Textarea(attrs={'cols':80, 'rows':2})
+        }
