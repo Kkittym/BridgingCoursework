80383d164e940efd828d56151bfff8659e016822
KJM891 <kjm891@student.bham.ac.uk>
2020-05-27 11:19:23 +0100

    Unit tests for elements of the page

--- CV/models.py
+++ CV/models.py
@@ -1,3 +1,35 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
-# Create your models here.
+class CV(models.Model):
+    name = models.TextField()
+    phone = models.CharField(max_length=11)
+    email = models.TextField()
+
+    def __str__(self):
+        return (self.name + "'s CV")
+
+class Section(models.Model):
+    CV = models.ForeignKey('CV.CV', on_delete=models.CASCADE, related_name='sections')
+    title = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class Institute(models.Model):
+    start = models.TextField()
+    end = models.TextField()
+    location = models.TextField()
+    area = models.TextField()
+
+class Element(models.Model):
+    section = models.ForeignKey(ContentType, on_delete=models.CASCADE, related_name='elements')
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('section', 'object_id')
+    text = models.TextField()
+
+    def __str__(self):
+        return ("Element for section: " + self.section.__str__() + ", text: " + self.text)
--- CV/tests.py
+++ CV/tests.py
@@ -1,9 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
-
 from django.urls import resolve
 from django.http import HttpRequest
+from django.contrib.auth.models import User
 
 from CV.views import main
 
@@ -16,4 +15,20 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/cv')
         self.assertTemplateUsed(response, 'main.html')
+
+    def test_education_on_page(self):
+        response = self.client.get('/cv')
+        self.assertIn('Education', response.content.decode())
+
+    def test_work_experience_on_page(self):
+        response = self.client.get('/cv')
+        self.assertIn('Work Experience', response.content.decode())
+
+    def test_other_skills_on_page(self):
+        response = self.client.get('/cv')
+        self.assertIn('Other Skills', response.content.decode())
+
+class AdminTestCase(TestCase):
+    def setUp(self):
+        User.objects.create_superuser()
 
