14469d76e77566884d1b59c54b130337b65ae930
KJM891 <kjm891@student.bham.ac.uk>
2020-06-07 17:43:56 +0100

    section tests

--- CV/tests.py
+++ CV/tests.py
@@ -66,37 +66,91 @@ class HomePageTest(TestCase):
 
         self.assertIn('This is a institute element test', response.content.decode())
 
-    def test_post_new_section_appears_on_page(self):
-        CV.objects.create(name='Test Test', phone='12345678910', email='test.test@test.com')
-        self.assertEqual(CV.objects.count(), 1)
-        self.client.post('/cv', data={'title':'Post Test'})
-        self.assertEqual(Section.objects.count(), 1)
-        response = self.client.get('/cv')
-        self.assertIn('Post Test', response.content.decode())
-
     def test_add_section_button_not_on_page(self):
         response = self.client.get('/cv')
         self.assertNotIn('Add section', response.content.decode())
 
+class UnauthedUserTest(TestCase):
+
+    def test_new_cv_url_redirects_login(self):
+        response = self.client.get('/cv/new')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/accounts/login/?next=/cv/new')
+
+    def test_new_section_url_redirects_login(self):
+        response = self.client.get('/cv/1/new_section')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/accounts/login/?next=/cv/1/new_section')
+
 class NewCvTest(TestCase):
+
+    def login(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        login = self.client.login(username='testuser', password='12345')
 
     def test_create_cv_resolves_to_new_cv_view(self):
         found = resolve('/cv/new')  
         self.assertEqual(found.func, CV_new)
 
     def test_redirects_after_POST(self):
+        self.login()
+
         response = self.client.post('/cv/new', data={'name':'Post Test', 'phone':'12345678910', 'email':'post.test@test.com'})
+
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/cv')
 
     def test_post_new_cv_saved(self):
+        self.login()
+
         self.client.post('/cv/new', data={'name':'Post Test', 'phone':'12345678910', 'email':'post.test@test.com'})
+
         self.assertEqual(CV.objects.count(), 1)
         new = CV.objects.first()
         self.assertEqual(new.name, 'Post Test')
         self.assertEqual(new.phone, '12345678910')
         self.assertEqual(new.email, 'post.test@test.com')
 
+    def test_post_new_cv_displayed(self):
+        self.login()
 
-#Section tests:
-# - Post on /cv redirects to section
+        self.client.post('/cv/new', data={'name':'Post Test', 'phone':'12345678910', 'email':'post.test@test.com'})
+
+        response = self.client.get('/cv')
+
+        self.assertIn('Post Test', response.content.decode())
+        self.assertIn('12345678910', response.content.decode())
+        self.assertIn('post.test@test.com', response.content.decode())
+
+    def test_user_gone(self):
+        self.assertEqual(User.objects.count(), 0)
+
+class SectionTest(TestCase):
+
+    def loginSetUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        login = self.client.login(username='testuser', password='12345')
+        CV.objects.create(name='Test Test', phone='12345678910', email='test.test@test.com')
+
+    def test_redirects_after_POST(self):
+        self.loginSetUp()
+        response = self.client.post('/cv/1/new_section', data={'title':'Section Post Test'})
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/cv/1/edit_section/1')
+
+    def test_post_new_section_saved(self):
+        self.loginSetUp()
+        self.client.post('/cv/1/new_section', data={'title':'Section Post Test'})
+
+        self.assertEqual(Section.objects.count(), 1)
+        new = Section.objects.first()
+        self.assertEqual(new.title, 'Section Post Test')
+
+    def test_post_new_section_appears_on_page(self):
+        self.loginSetUp()
+        self.client.post('/cv/1/new_section', data={'title':'Section Post Test'})
+        
+        response = self.client.get('/cv')
+        self.assertIn('Section Post Test', response.content.decode())
+
